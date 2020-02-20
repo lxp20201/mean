e08864b24dd3cee6a50735ab626ef1a04b2a3f08
rahul <rahulsai.vishnu@cintanatech.com>
2020-02-20 14:44:16 +0530

    is_staff check added

--- routes/registration/track.service.js
+++ routes/registration/track.service.js
@@ -50,6 +50,12 @@ let update_status = async (data) => {
 let externalregistration = async (request) => {
   try {
     var payload = request;
+    if(request.is_staff == true){
+      payload.is_staff = 1
+    }
+    else if(request.is_staff == false){
+      payload.is_staff = 0
+    }
     var response = await invoke.makeHttpCallpolyglot("post", "/user_api/v1/account/registration/", payload);
     console.log(response.data);
     if (response.data.success == true) {
